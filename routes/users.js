a1d9a9fdaaaacc02c2719fe4ca568fb47c5923b8
Vedant Gupta <vedantgupta523@gmail.com>
2021-03-14 22:10:02 -0400

    whohoo

--- routes/users.js
+++ routes/users.js
@@ -19,65 +19,65 @@ passport.authenticate('local',{
     failureFlash : true
 })(req,res,next)
 })
-  //register post handle
-  router.post('/register',(req,res)=>{
-    const {name,email, password, password2} = req.body;
-    let errors = [];
-    console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
-    if(!name || !email || !password || !password2) {
-        errors.push({msg : "Please fill in all fields"})
-    }
-    //check if match
-    if(password !== password2) {
-        errors.push({msg : "Passwords don't match"});
-    }
-    
-    //check if password is more than 6 characters
-    if(password.length < 6 ) {
-        errors.push({msg : 'Password at least 6 characters'})
-    }
-    if(errors.length > 0 ) {
-    res.render('register', {
-        errors : errors,
-        name : name,
-        email : email,
-        password : password,
-        password2 : password2})
-     } else {
-        //validation passed
-       User.findOne({email : email}).exec((err,user)=>{
-        console.log(user);   
-        if(user) {
-            errors.push({msg: 'Email already registered'});
-            res.render('register',{errors,name,email,password,password2})  
-           } else {
-            const newUser = new User({
-                name : name,
-                email : email,
-                password : password
-            });
-    
-            //hash password
-            bcrypt.genSalt(10,(err,salt)=> 
-            bcrypt.hash(newUser.password,salt,
-                (err,hash)=> {
-                    if(err) throw err;
-                        //save pass to hash
-                        newUser.password = hash;
-                    //save user
-                    newUser.save()
-                    .then((value)=>{
-                        console.log(value)
-                        req.flash('success_msg','You have now registered!');
-                        res.redirect('/users/login');
-                    })
-                    .catch(value=> console.log(value));
-                      
-                }));
-             }
-       })
-    }
+//register post handle
+router.post('/register',(req,res)=>{
+const {name,email, password, password2} = req.body;
+let errors = [];
+console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
+if(!name || !email || !password || !password2) {
+    errors.push({msg : "Please fill in all fields"})
+}
+//check if match
+if(password !== password2) {
+    errors.push({msg : "Passwords don't match"});
+}
+
+//check if password is more than 6 characters
+if(password.length < 6 ) {
+    errors.push({msg : 'Password at least 6 characters'})
+}
+if(errors.length > 0 ) {
+res.render('register', {
+    errors : errors,
+    name : name,
+    email : email,
+    password : password,
+    password2 : password2})
+    } else {
+    //validation passed
+    User.findOne({email : email}).exec((err,user)=>{
+    console.log(user);   
+    if(user) {
+        errors.push({msg: 'Email already registered'});
+        res.render('register',{errors,name,email,password,password2})  
+        } else {
+        const newUser = new User({
+            name : name,
+            email : email,
+            password : password
+        });
+
+        //hash password
+        bcrypt.genSalt(10,(err,salt)=> 
+        bcrypt.hash(newUser.password,salt,
+            (err,hash)=> {
+                if(err) throw err;
+                    //save pass to hash
+                    newUser.password = hash;
+                //save user
+                newUser.save()
+                .then((value)=>{
+                    console.log(value)
+                    req.flash('success_msg','You have now registered!');
+                    res.redirect('/users/login');
+                })
+                .catch(value=> console.log(value));
+                    
+            }));
+            }
     })
+}
+})
 
 // project
 router.get('/dashboard',(req,res)=>{
@@ -98,6 +98,18 @@ router.post('/dashboard',(req,res)=>{
     })
     .catch(value=> console.log(value));
 })
+
+//preview
+router.get('/portfolio/:id',(req,res)=>{
+    User.findById(req.params.id).exec((err,user)=>{
+        Project.find({'user_id' : req.params.id}).exec((err,projects)=>{
+            res.render('portfolio',{
+                user: user,
+                projects: projects
+            });  
+        })
+    })
+    })
 
 //logout
 router.get('/logout',(req,res)=>{
